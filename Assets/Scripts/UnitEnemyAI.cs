178e3b87c7b545fa61ced32e2eb90ebfd9e37918
Thais Correia <tnmcorreia@gmail.com>
2013-12-08 17:23:19 -0400

    Navmesh Agent Component

--- Assets/Scripts/UnitEnemyAI.cs
+++ Assets/Scripts/UnitEnemyAI.cs
@@ -4,34 +4,37 @@ using System.Collections;
 public class UnitEnemyAI : Unit {
 	
 	public Transform target;
-	protected float moveSpeed = 4f;
+	protected float moveSpeed;
 	protected float rotationSpeed = 2.0f;
 	protected bool escape = false;
+	NavMeshAgent agent;
 
 	public override void Start () {
+		
+		moveSpeed = 4f;
+		agent = GetComponent<NavMeshAgent>();
 		
 	}
 	
 	public override void Update () 
 	{
 		i++;
-		Vector3 point;
-		if(!tagged)
-		{
-			point = new Vector3(target.position.x, 0f, target.position.z);
+        Vector3 point;
+        if(!tagged)
+        {
+            point = new Vector3(target.position.x, transform.position.y, target.position.z);
 			transform.LookAt(point);
-			escape = false;
-		}
-		else
-		{
-			Vector3 position = new Vector3(transform.position.x, 0f, transform.position.z);
-			Vector3 tPosition = new Vector3(target.position.x, 0f, target.position.z);
-			point = 2.0f * position - tPosition;
-    		transform.LookAt(point);
-		}
+            escape = false;
+        }
+        else
+        {
+            Vector3 position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            Vector3 tPosition = new Vector3(target.position.x, transform.position.y, target.position.z);
+            point = 2.0f * position - tPosition;
+			transform.LookAt(point);
+        }
 		
-		transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-		
+		agent.destination = point;
 	}
 	
 	void OnCollisionEnter(Collision collision)
@@ -39,6 +42,7 @@ public class UnitEnemyAI : Unit {
 		if(collision.transform == target && i >= 50){
 			tagged = true;
 			print("Enemy says: Catch me if you can!");
+			i = 0;
 		}
 	}
 }
